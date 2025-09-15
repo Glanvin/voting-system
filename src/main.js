8992c978e28357da613459243834e8fd02ba47ad
Glanvin <110996095+Glanvin@users.noreply.github.com>
2025-09-15 12:48:22 +0800

    ESlint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import './style.css'queueMicrotask
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello World</h1>
+  <h2>world, Hello</h2>
 `
 
 setupCounter(document.querySelector('#counter'))
